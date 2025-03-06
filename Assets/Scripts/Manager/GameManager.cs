4a8d2081b48b0dd5425dc35f46749f353e89ac9c
Yuanhao <scyyd8@nottingham.edu.cn>
2025-03-06 16:50:58 +0800

    修复版本bug

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class sc : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         AudioManager.instance.PlayBGM(0);
-        Debug.Log($"²¥·ÅÒôÀÖ");
+        Debug.Log($"ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½");
     }
 
     // Update is called once per frame

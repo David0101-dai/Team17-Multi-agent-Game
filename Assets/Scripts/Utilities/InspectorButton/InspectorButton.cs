5da8628facd189610d877aac26582e718e4d2620
Zihan Yu <scyzy6@nottingham.edu.cn>
2025-03-08 16:10:51 +0800

    添加工程文件CTKILLER

--- Assets/Scripts/Utilities/InspectorButton/InspectorButton.cs
+++ Assets/Scripts/Utilities/InspectorButton/InspectorButton.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System;
 using System.Collections;
 using System.Linq;
@@ -58,3 +59,4 @@ public class InspectorButton : Editor
         EditorGUILayout.EndHorizontal();
     }
 }
+#endif

1fd1e347b56392e06e22daa49d6bd48c25d32474
Yuanhao <scyyd8@nottingham.edu.cn>
2025-03-17 01:45:15 +0800

    史莱姆分裂

--- Assets/Scripts/Character/Enemy/史莱姆/Slime.cs
+++ Assets/Scripts/Character/Enemy/史莱姆/Slime.cs
@@ -1,8 +1,18 @@
 using System.Collections;
 using UnityEngine;
 
+public enum SlimeType {big,middle,small}
 public class Slime : Enemy
 {
+    private bool isDead;
+
+    [Header("Slime spesific")]
+    [SerializeField] private SlimeType SlimeType;
+    [SerializeField] private int amountOfSlimeToCreat;
+    [SerializeField] private GameObject SlimePrefab;
+    [SerializeField] private Vector2 minCreationVelocity;
+    [SerializeField] private Vector2 maxCreationVelocity;
+
     #region State
     public IState IdleState { get; private set; }
     public IState PatrolState { get; private set; }
@@ -25,6 +35,7 @@ public class Slime : Enemy
         DeadState = new SlimeDeadState(Fsm, this, "Dead");
         StunState = new SlimeStunState(Fsm, this, "Stun");
         Fsm.SwitchState(IdleState);
+        isDead = false;
     }
 
     protected override void Update()
@@ -50,16 +61,62 @@ public class Slime : Enemy
 
     public override void Die()
     {
-        if (this.gameObject.name == "BigSkeleton")
-        {
-            PlayerManager.Instance.AddScore(4);
+        if(isDead){
+            return;
         }
-        else
-        {
-            PlayerManager.Instance.AddScore(2);
-        }
+
+        isDead = true;
+
+        PlayerManager.Instance.AddScore(2);
+
         Debug.Log(PlayerManager.finalscore);
+
         Fsm.SwitchState(DeadState);
+
+        if(SlimeType ==SlimeType.small){
+            return;
+        }else{
+            CreateSlimes(amountOfSlimeToCreat, SlimePrefab);
+        }
+
+    }
+
+    private void CreateSlimes(int _amountOfSlime, GameObject _slimePrefab)
+    {
+        for (int i = 0; i < _amountOfSlime; i++)
+        {
+            // 在原本的位置上加上随机偏移量
+            float xOffset = Random.Range(-1f, 1f);  // 控制水平偏移范围
+            float yOffset = Random.Range(0.5f, 2f); // 控制垂直偏移范围
+
+            Vector3 spawnPosition = transform.position + new Vector3(xOffset, yOffset, 0);  // 增加偏移量
+
+            GameObject newSlime = Instantiate(_slimePrefab, spawnPosition, Quaternion.identity);
+
+            newSlime.GetComponent<Slime>().SetUpSlime(Flip.facingDir);
+        }
+    }
+
+
+    public void SetUpSlime(int _facingDir){
+
+        // 获取 FlipSprite 组件
+    FlipSprite flipSprite = GetComponent<FlipSprite>();
+
+    if (flipSprite == null)
+    {
+        Debug.LogError("FlipSprite component is missing on the Slime object!");
+        return;
+    }
+
+        if(_facingDir != flipSprite.facingDir){
+            flipSprite.Flip();
+        }
+
+        float xVelocity = Random.Range(minCreationVelocity.x,maxCreationVelocity.x);
+        float yVelocity = Random.Range(minCreationVelocity.y,maxCreationVelocity.y);
+        
+        GetComponent<Rigidbody2D>().velocity = new Vector2(-flipSprite.facingDir*xVelocity,yVelocity);
     }
 
     public void DestroyMe()
